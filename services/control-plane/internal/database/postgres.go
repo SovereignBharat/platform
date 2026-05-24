9947b89f1e3f1c92121dbdebc3160fba5d5e30c5
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-23 21:17:57 -0700

    control-plane: add postgres connection layer

---
+++ services/control-plane/internal/database/postgres.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func Connect(url string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	fmt.Println("connected to postgres")
+	return db, nil
+}
